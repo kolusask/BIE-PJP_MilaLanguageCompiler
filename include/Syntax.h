b8b9f1a649295519d141446ec74ca597a416bdba
kolusask <19ak97@gmail.com>
2020-04-29 19:59:43 +0200

    Added mapping from token type to string for simple tokens

--- include/Syntax.h
+++ include/Syntax.h
@@ -15,11 +15,13 @@ class Syntax {
 public:
     static TokenType check_character(const char op);
     static TokenType check_keyword(const std::string& word);
+    static std::string simple_token(const TokenType type);
     static bool is_delimiter(const char del);
 private:
     static const std::set<char> s_Delimiters;
     static const std::map<std::string, TokenType> s_KeyWords;
     static const std::map<char, TokenType> s_Characters;
+    static const std::map<TokenType, std::string> s_SimpleTokens;
 };
 
 const std::set<char> Syntax::s_Delimiters = { ' ', '\n', '\t' };
@@ -34,6 +36,20 @@ const std::map<char, TokenType> Syntax::s_Characters = {{'=', TOK_ASSIGN},
                                                         {';', TOK_SEMICOLON},
                                                         {'(', TOK_OPEN_BRACKET},
                                                         {')', TOK_CLOSE_BRACKET}};
+
+const std::map<TokenType, std::string> Syntax::s_SimpleTokens = {{TOK_ASSIGN, "="},
+                                                                 {TOK_BEGIN, "begin"},
+                                                                 {TOK_CLOSE_BRACKET, ")"},
+                                                                 {TOK_CONST, "const"},
+                                                                 {TOK_DOT, "."},
+                                                                 {TOK_END, "end"},
+                                                                 {TOK_OPEN_BRACKET, ")"},
+                                                                 {TOK_PROGRAM, "program"},
+                                                                 {TOK_SEMICOLON, ";"}};
+
+std::string Syntax::simple_token(const TokenType type) {
+    return s_SimpleTokens.at(type);
+}
 
 TokenType Syntax::check_character(const char op) {
     if (s_Characters.count(op))
