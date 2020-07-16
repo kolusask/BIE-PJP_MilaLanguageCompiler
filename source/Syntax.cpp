7a7df72f810c41352772b0430241485edd315737
kolusask <19ak97@gmail.com>
2020-07-16 18:03:36 +0200

    Refactored Syntax class

--- source/Syntax.cpp
+++ source/Syntax.cpp
@@ -4,34 +4,34 @@
 
 #include "../include/Syntax.h"
 
-const std::set<char> Syntax::s_delimiters = { ' ', '\n', '\t' };
 
-const std::map<std::string, TokenType> Syntax::s_keyWords = {{"begin", TOK_BEGIN},
-                                                             {"const", TOK_CONST},
-                                                             {"end", TOK_END},
-                                                             {"program", TOK_PROGRAM}
-                                                            };
-
-const std::map<char, TokenType> Syntax::s_characters = {{'=', TOK_INIT},
-                                                        {'.', TOK_DOT},
-                                                        {';', TOK_SEMICOLON},
-                                                        {'(', TOK_OPEN_BRACKET},
-                                                        {')', TOK_CLOSE_BRACKET},
-                                                        {',', TOK_COMMA}
-                                                        };
-
-TokenType Syntax::check_character(const char op) {
-    if (s_characters.count(op))
-        return s_characters.at(op);
+TokenType Syntax::check_character(const char ch) {
+    static const std::map<char, TokenType> characters = {{'=', TOK_INIT},
+                                                         {'.', TOK_DOT},
+                                                         {';', TOK_SEMICOLON},
+                                                         {'(', TOK_OPEN_BRACKET},
+                                                         {')', TOK_CLOSE_BRACKET},
+                                                         {',', TOK_COMMA}};
+    auto it = characters.find(ch);
+    if (it != characters.end())
+        return it->second;
     return TOK_INVALID;
 }
 
 TokenType Syntax::check_keyword(const std::string &word) {
-    if (s_keyWords.count(word))
-        return s_keyWords.at(word);
+    static const std::map<std::string, TokenType> keyWords = {{"begin", TOK_BEGIN},
+                                                              {"const", TOK_CONST},
+                                                              {"end", TOK_END},
+                                                              {"program", TOK_PROGRAM}};
+    auto it = keyWords.find(word);
+    if (it != keyWords.end())
+        return it->second;
     return TOK_INVALID;
 }
 
 bool Syntax::is_delimiter(const char del) {
+    static const std::set<char> delimiters = { ' ',
+                                               '\n',
+                                               '\t' };
     return s_delimiters.count(del);
 }
